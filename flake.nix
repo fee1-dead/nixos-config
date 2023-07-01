48c8a0e087f88070e8a316d71cf7f56b78313dd1
Deadbeef <ent3rm4n@gmail.com>
2023-07-01 08:15:17 +0800

    up

--- flake.nix
+++ flake.nix
@@ -2,12 +2,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
-    typst-lsp.url = "github:nvarner/typst-lsp";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, home-manager, ... } @inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager {
