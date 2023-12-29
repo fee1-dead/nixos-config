3eee6f69e9495ce4da61ec446f885c46120f5f12
Deadbeef <ent3rm4n@gmail.com>
2023-12-30 01:51:21 +0800

    up

--- per-system/pc.nix
+++ per-system/pc.nix
@@ -3,4 +3,5 @@
   networking.hostName = "uwu"; # Define your hostname.
   system.stateVersion = "24.05";
   nix.settings.substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+  systemd.services.nix-daemon.environment.all_proxy = "socks5h://192.168.1.7:7891";
 }
