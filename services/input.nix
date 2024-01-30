645f370502fd6da62aa77807169d610ed4d64a8b
Deadbeef <ent3rm4n@gmail.com>
2024-01-30 18:25:46 +0800

    up

--- services/input.nix
+++ services/input.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
   i18n.inputMethod.enabled = "fcitx5";
   i18n.inputMethod.fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-mozc fcitx5-hangul ];
+  environment.sessionVariables = {
+    NIX_PROFILES =
+      "${lib.concatStringsSep " " (lib.reverseList config.environment.profiles)}";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
 }
