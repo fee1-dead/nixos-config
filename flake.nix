519c1d629fdb93b406ef8bc9f44dad48b1037c8c
Deadbeef <ent3rm4n@gmail.com>
2024-11-25 13:06:40 +0800

    u

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     lix-module = {
