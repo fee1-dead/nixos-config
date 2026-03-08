f433c89f78a4eff32f318a495d7dce7b29c25546
Deadbeef <ent3rm4n@gmail.com>
2026-03-08 13:39:36 -0400

    rm steam

--- usage.nix
+++ usage.nix
@@ -11,11 +11,11 @@
     };
     supportedFilesystems = [ "ntfs" ];
   };
-  programs.steam = {
+  /*programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
+  };*/
   environment.systemPackages = with pkgs; [
     prismlauncher
     pear-desktop
